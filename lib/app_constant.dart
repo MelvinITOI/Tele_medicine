fba4be6ef8dea6c42853f96812cc527e99d6f737
MelvinITOI <melvin@itoi.org>
2023-10-19 11:30:04 +0530

    Add loader fun in app constant

--- lib/app_constant.dart
+++ lib/app_constant.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/Material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 
 
@@ -29,3 +30,21 @@ void Snackbar_Successfully(BuildContext context) {
   ));
 }
 //-----------------------------------------------------------------------***************----------------------------------------------------------------------------------------//
+// LOADER
+void configLoading() {
+  EasyLoading.instance
+    ..displayDuration = const Duration(milliseconds: 2000)
+    ..indicatorType = EasyLoadingIndicatorType.ring
+    ..loadingStyle = EasyLoadingStyle.dark
+    ..indicatorSize = 45.0
+    ..radius = 10.0
+    ..progressColor = Colors.yellow
+    ..backgroundColor = Colors.green
+    ..indicatorColor = Colors.yellow
+    ..textColor = Colors.yellow
+    ..maskColor = Colors.blue.withOpacity(0.5)
+    ..userInteractions = true
+    ..dismissOnTap = false;
+  // ..customAnimation = CustomAnimation();
+}
+//---------------------------------------------------------------------*****************-----------------------------------------------------------------------------------------/
